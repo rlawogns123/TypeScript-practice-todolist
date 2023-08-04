2d83dff5712273770ec19098881e2ecbe403a5fe
rmawogns <evolui@naver.com>
2023-08-04 16:45:53 +0900

    addTodo, deleteTodo, toggleDone 함수 추가

--- todolist-app/src/App.tsx
+++ todolist-app/src/App.tsx
@@ -1,7 +1,42 @@
 import { useState } from "react";
+import { produce } from "immer";
+
+export type TodoListItemType = {
+  no: number;
+  todo: string;
+  done: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState<Array<TodoListItemType>>([
+    { no: 1, todo: "일어나기", done: false },
+    { no: 2, todo: "물마시기", done: false },
+    { no: 3, todo: "세수하기", done: false },
+    { no: 4, todo: "산책하기", done: false },
+  ]);
+
+  const addTodo = (todo: string) => {
+    let newTodoList = produce(todoList, (draft) => {
+      draft.push({ no: new Date().getTime(), todo: todo, done: false });
+    });
+    setTodoList(newTodoList);
+  };
+
+  const deleteTodo = (no: number) => {
+    let index = todoList.findIndex((todo) => todo.no === no);
+    let newTodoList = produce(todoList, (draft) => {
+      draft.splice(index, 1);
+    });
+    setTodoList(newTodoList);
+  };
+
+  const toggleDone = (no: number) => {
+    let index = todoList.findIndex((todo) => todo.no === no);
+    let newTodoList = produce(todoList, (draft) => {
+      draft[index].done = !draft[index].done;
+    });
+    setTodoList(newTodoList);
+  };
 
   return <div>todoList</div>;
 }
