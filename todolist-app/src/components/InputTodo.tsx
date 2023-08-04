541e0a121ab1ac3b0051baccc2f2772fe15ce7e7
rmawogns <evolui@naver.com>
2023-08-04 17:43:35 +0900

    InputTodo 구현

--- todolist-app/src/components/InputTodo.tsx
+++ todolist-app/src/components/InputTodo.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 type InputTodoProps = {
   addTodo: (todo: string) => void;
 };
 
 const InputTodo = (props: InputTodoProps) => {
-  return <div>InputTodo</div>;
+  const [todo, setTodo] = useState<string>("");
+
+  const addHandler = () => {
+    props.addTodo(todo);
+    setTodo("");
+  };
+
+  const enterInput = (e: React.KeyboardEvent) => {
+    if (e.key === "Enber") {
+      addHandler();
+    }
+  };
+
+  const changeTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="할 일 입력"
+        value={todo}
+        onChange={changeTodo}
+        onKeyUp={enterInput}
+      />
+      <span onClick={addHandler}>추가</span>
+    </div>
+  );
 };
 
 export default InputTodo;
